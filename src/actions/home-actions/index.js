7673dfdacdd3bb7aef9cbe2598fe087a141dc060
Jonathan Boggon <jonathanboggon@hotmail.com>
2020-04-11 09:20:45 +0100

    actions

---
+++ src/actions/home-actions/index.js
@@ -0,0 +1,35 @@
+import { api, prependRequest } from '../../lib/constants';
+import { FETCH_HOME, FETCH_HOME_SUCCESS, FETCH_HOME_ERROR } from './types';
+
+function fetchHome() {
+  return {
+    type: FETCH_HOME
+  };
+}
+
+function fetchHomeSuccess(data) {
+  return {
+    type: FETCH_HOME_SUCCESS,
+    data
+  };
+}
+
+function fetchHomeError() {
+  return {
+    type: FETCH_HOME_ERROR
+  };
+}
+
+export const requestHomeApi = `${prependRequest}/${api}/pages?slug=home&_fields=acf`;
+
+export function requestHome() {
+  return dispatch => {
+    dispatch(fetchHome());
+    return fetch(requestHomeApi)
+      .then(response => response.json())
+      .then(
+        data => dispatch(fetchHomeSuccess(data)),
+        () => dispatch(fetchHomeError())
+      );
+  };
+}
---
+++ src/actions/projects-actions/index.js
@@ -0,0 +1,42 @@
+import { api,prependRequest } from '../../lib/constants';
+import {
+  FETCH_PROJECTS,
+  FETCH_PROJECTS_SUCCESS,
+  FETCH_PROJECTS_ERROR
+} from './types';
+
+function fetchProjects() {
+  return {
+    type: FETCH_PROJECTS
+  };
+}
+
+function fetchProjectsSuccess(data) {
+  return {
+    type: FETCH_PROJECTS_SUCCESS,
+    data
+  };
+}
+
+function fetchProjectsError() {
+  return {
+    type: FETCH_PROJECTS_ERROR
+  };
+}
+
+const requestProjectsApi = `${prependRequest}/${api}/project?_fields=title,acf,slug`;
+
+export const getRequestProjectsApi = slug =>
+  slug ? `${requestProjectsApi}&slug=${slug}` : requestProjectsApi;
+
+export function requestProjects(slug) {
+  return dispatch => {
+    dispatch(fetchProjects());
+    return fetch(getRequestProjectsApi(slug))
+      .then(response => response.json())
+      .then(
+        data => dispatch(fetchProjectsSuccess(data)),
+        () => dispatch(fetchProjectsError())
+      );
+  };
+}
