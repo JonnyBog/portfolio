9c77798e6da43f96e5273c54ce920e6ba1f2a902
JonnyBog <jonathan.boggon@clearscore.com>
2020-04-13 08:29:56 +0000

    Deploying to gh-pages from  @ 476c9b44bbab57f97fd300bf1165f7bbf33e83c8 🚀

---
+++ precache-manifest.15f9f9fabd855a3c3826da66f44d6192.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7af3c8d728a5f7c50304e0be61702fcd",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e7741688f4bf002a8f0f",
+    "url": "/portfolio/static/css/main.79a439df.chunk.css"
+  },
+  {
+    "revision": "3bc7e0aef9034216d0eb",
+    "url": "/portfolio/static/js/2.e8b1ff13.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio/static/js/2.e8b1ff13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7741688f4bf002a8f0f",
+    "url": "/portfolio/static/js/main.41228c76.chunk.js"
+  },
+  {
+    "revision": "6c1d3ad26a766dc494c1",
+    "url": "/portfolio/static/js/runtime-main.d597d31e.js"
+  }
+]);
